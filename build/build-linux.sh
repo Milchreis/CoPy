e7cfd98c88f95193a2b8862a768282f648d99657
Nick <nick-m@gmx.de>
2015-08-14 20:22:44 +0200

    Correction

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -1,5 +1,5 @@
-pyinstaller-2.0/pyinstaller.py -F -m manifest.xml -n PicSort -w -o ../bin/ PicSort.spec
-cp ./dist/PicSort ../bin/PicSort
+pyinstaller-2.0/pyinstaller.py -F -m manifest.xml -n CoPy -w -o ../bin/ CoPy.spec
+cp ./dist/CoPy ../bin/CoPy
 
 rm -r ./build
 rm -r ./dist
